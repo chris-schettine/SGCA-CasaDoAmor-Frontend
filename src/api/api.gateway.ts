011b5f125e5885a9d31bb242c5115510ce627321
henriksson666 <j.henrique.uesb@gmail.com>
2025-09-24 08:11:21 -0300

    Update .gitignore to include node_modules; fix package-lock.json version; refactor TablePatients and TableUsers components for consistency

--- src/api/api.gateway.ts
+++ src/api/api.gateway.ts
@@ -1,7 +1,7 @@
 import axios, { type AxiosInstance } from 'axios';
 import type { PessoaFisicaDTO } from './api.gateway.dto';
 
-// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8090";
+//const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8090";
 
 class ApiGateway {
   public gateway: AxiosInstance;
