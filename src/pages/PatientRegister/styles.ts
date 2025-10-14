738f5b0bb326df6776c9b637d8bf8ecd9fa4807c
Luis Eduardo da Silva Santos <luisdudu2020@gmail.com>
2025-10-14 13:36:33 -0300

    Configurações do vite inserindo headers de segurança e ajuste de estilos no formulário paciente

--- src/pages/PatientRegister/styles.ts
+++ src/pages/PatientRegister/styles.ts
@@ -27,10 +27,10 @@ export const buttonStyles = css({
 });
 
 export const saveButtonStyles = css({
-  backgroundColor: '#000', // Cor preta para o botão Salvar
+  backgroundColor: '#09244B', // Cor azul escuro para o botão salvar
   color: '#fff',
   '&:hover': {
-    backgroundColor: '#333',
+    backgroundColor: '#0C2F58',// um tom mais claro do azul escuro para melhorar o hover;
   },
 });
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+    headers:{
+      //Tem a prevenção para o site não ser renderizado dentro de um <inframe>
+      'X-Frame-Options': 'DENY',
+      //Impedi o navegador de executar um arquivo de texto que pode conter um script
+      'X-Content-Type-Options': 'nosniff',
+      //Politica de Segurança de Conteúdo para previni sobre ataques de XSS (Cross-Site Scripting)
+      'Content-Security-Policy': "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'sefl' data:; front-src 'self';"
+    }
+  }
 })
