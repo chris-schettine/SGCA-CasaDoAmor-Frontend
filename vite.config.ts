b089f0e46e46b128fddd70488f2d1d27edaa6b37
João Henrique Silva Pinto <j.henrique.uesb@gmail.com>
2025-10-22 20:10:47 -0300

    fix: update Content Security Policy to include additional connect-src for external service

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
       //Politica de Segurança de Conteúdo para previni sobre ataques de XSS (Cross-Site Scripting)
       // Relaxed for local development so Vite's HMR and injected inline scripts/images work.
       // In production you should set a stricter CSP (and avoid 'unsafe-inline').
-      'Content-Security-Policy': "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' http://localhost:5173; style-src 'self' 'unsafe-inline'; img-src 'self' data: http://localhost:5173; font-src 'self'; connect-src 'self' ws://localhost:5173;"
+      'Content-Security-Policy': "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' http://localhost:5173; style-src 'self' 'unsafe-inline'; img-src 'self' data: http://localhost:5173; font-src 'self'; connect-src 'self' ws://localhost:5173 http://144.22.182.60:8888;"
     }
   }
 })
